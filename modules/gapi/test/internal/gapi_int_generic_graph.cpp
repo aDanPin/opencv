917a38d1bbd3ec6bf8c4b56908ee587184776f00
aDanPin <drobrorobro@gmail.com>
2020-07-02 17:54:37 +0300

    Add all tests

--- modules/gapi/test/internal/gapi_int_generic_graph.cpp
+++ modules/gapi/test/internal/gapi_int_generic_graph.cpp
@@ -11,7 +11,33 @@
 
 namespace opencv_test
 {
-    TEST(GenericGraph, NoRecompileWithSameMeta)
+    TEST(GenericGraph, BazeFunctianal1)
+    {
+        cv::GMat out1;
+        cv::GProtoOutputArgs outs = GOut(out1);
+
+        cv::GMat out2;
+        EXPECT_NO_THROW(outs += GOut(out2));
+    }
+
+    TEST(GenericGraph, BazeFunctianal2)
+    {
+        cv::GMat in1;
+        cv::GProtoInputArgs ins = GIn(in1);
+
+        cv::GMat in2;
+        EXPECT_NO_THROW(ins += GIn(in2));
+    }
+
+    TEST(GenericGraph, BazeFunctianal3)
+    {
+        cv::Mat out_mat1, out_mat2;
+        cv::GRunArgsP out_vector;
+        EXPECT_NO_THROW(out_vector += cv::gout(out_mat1));
+        EXPECT_NO_THROW(out_vector += cv::gout(out_mat2));
+    }
+
+    TEST(GenericGraph, GOut)
     {
         cv::GComputation cc([]() {
             cv::GMat in;
@@ -31,7 +57,7 @@ namespace opencv_test
         EXPECT_NO_THROW(cc.apply(cv::gin(in_mat1), cv::gout(out_mat1, out_mat1)));
     }
 
-    TEST(GenericGraph, NoRecompileWithSameMeta1)
+    TEST(GenericGraph, GIn)
     {
         cv::GComputation cc([]() {
             cv::GMat in1;
@@ -52,7 +78,7 @@ namespace opencv_test
         EXPECT_NO_THROW(cc.apply(cv::gin(in_mat1, in_mat2), cv::gout(out_mat)));
     }
 
-    TEST(GenericGraph, NoRecompileWithSameMeta2)
+    TEST(GenericGraph, GInGOut)
     {
         cv::GComputation cc([]() {
             cv::GMat in1;
@@ -77,7 +103,48 @@ namespace opencv_test
         EXPECT_NO_THROW(cc.apply(cv::gin(in_mat1, in_mat2), cv::gout(out_mat1, out_mat2)));
     }
 
-    TEST(GenericGraph, NoRecompileWithSameMeta3)
+    TEST(GenericGraph, Accuracy)
+    {
+        cv::Size szOut(4, 4);
+        cv::GComputation cc([&](){
+            cv::GMat in1;
+            cv::GProtoInputArgs ins = GIn(in1);
+
+            cv::GMat in2;
+            ins += GIn(in2);
+
+            cv::GMat out1 = cv::gapi::resize(in1, szOut);
+            cv::GProtoOutputArgs outs = GOut(out1);
+
+            cv::GMat out2 = cv::gapi::resize(in2, szOut);
+            outs += GOut(out2);
+
+            return cv::GComputation(std::move(ins), std::move(outs));
+        });
+
+        // G-API test code
+        cv::Mat in_mat1( 8,  8, CV_8UC3);
+        cv::Mat in_mat2(16, 16, CV_8UC3);
+        cv::randu(in_mat1, cv::Scalar::all(0), cv::Scalar::all(255));
+        cv::randu(in_mat2, cv::Scalar::all(0), cv::Scalar::all(255));
+
+        cv::Mat out_mat1, out_mat2;
+        cv::GRunArgsP out_vector;
+        out_vector += cv::gout(out_mat1);
+        out_vector += cv::gout(out_mat2);
+
+        cc.apply(cv::gin(in_mat1, in_mat2), cv::gout(out_mat1, out_mat2));
+
+        // OCV ref code
+        cv::Mat cv_out_mat1, cv_out_mat2;
+        cv::resize(in_mat1, cv_out_mat1, szOut);
+        cv::resize(in_mat2, cv_out_mat2, szOut);
+
+        EXPECT_EQ(0, cvtest::norm(out_mat1, cv_out_mat1, NORM_INF));
+        EXPECT_EQ(0, cvtest::norm(out_mat2, cv_out_mat2, NORM_INF));
+    }
+
+    TEST(GenericGraph, Streaming)
     {
         cv::GComputation cc([&](){
             cv::Size szOut(4, 4);
@@ -100,7 +167,7 @@ namespace opencv_test
         EXPECT_NO_THROW(cc.compileStreaming(cv::compile_args(cv::gapi::core::cpu::kernels())));
     }
 
-    TEST(GenericGraph, NoRecompileWithSameMeta4)
+    TEST(GenericGraph, StreamingAccuracy)
     {
         cv::Size szOut(4, 4);
         cv::GComputation cc([&](){
